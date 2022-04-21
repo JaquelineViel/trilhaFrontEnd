da135cb235ef4895aa113c97b7bb384450cdfa71
Jaqueline <jaquelinecviel@gmail.com>
2022-04-21 02:51:29 -0300

    Desafio 06 - D4 Angular

---
+++ Desafio 06/prototipo/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule, Component } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ComponentsComponent } from './components/components.component';
+import { formcomponent } from './components/form/form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ComponentsComponent,
+    formcomponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Desafio 06/prototipo/src/app/forms.ts
@@ -0,0 +1,5 @@
+export interface form {
+    name: string,
+    subject: string,
+    message: string,
+}
